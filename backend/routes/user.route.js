f65988ae98c546e4c6f2d2eda9bf9ec2bc060a8f
Badal Chand <badalchand636@gmail.com>
2026-02-26 13:38:52 +0545

    Initial commit: MERN_JOB_PORTAL backend setup

---
+++ backend/routes/user.route.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { login, logout, register, updateProfile } from "../controllers/user.controller.js";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
+
+const userRouter = express.Router();
+
+userRouter.post("/register", register);
+userRouter.post("/login", login);
+userRouter.post("/profile/update", isAuthenticated, updateProfile);
+userRouter.post("/logout", logout);
+
+export default userRouter;
